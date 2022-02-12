28e22770f296109e9f6f9672ab150b66b5057505
HarlausG <73186172+HarlausG@users.noreply.github.com>
2022-02-12 15:32:39 +0100

    Ajout de l'API Weather

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+
+const request = require('request');
+
 app.use("/src", express.static('./src/'));
 
 app.get('/', (req, res) => {
@@ -19,4 +22,33 @@ io.on('connection', (socket) => {
 
 server.listen(3000, () => {
     console.log("listening on : 3000");
+});
+
+
+
+
+const options = {
+  method: 'GET',
+  url: 'https://community-open-weather-map.p.rapidapi.com/weather',
+  qs: {
+    q: 'London,uk',
+    lat: '0',
+    lon: '0',
+    callback: 'test',
+    id: '2172797',
+    lang: 'null',
+    units: 'imperial',
+    mode: 'xml'
+  },
+  headers: {
+    'x-rapidapi-host': 'community-open-weather-map.p.rapidapi.com',
+    'x-rapidapi-key': '4b4f95b09fmsh03d913b83287832p17f84cjsne750ead9d017',
+    useQueryString: true
+  }
+};
+
+request(options, function (error, response, body) {
+	if (error) throw new Error(error);
+
+	console.log(body);
 });
