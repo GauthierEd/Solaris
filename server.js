23f12754ab4e82c75f2da1cfd31ac4ff11951365
HarlausG <73186172+HarlausG@users.noreply.github.com>
2022-02-12 15:18:26 +0100

    Initialisation du serveur WebSocket

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+
+const http = require("http");
+const server = http.createServer(app);
+
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.use("/src", express.static('./src/'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log("a user connected");
+});
+
+server.listen(3000, () => {
+    console.log("listening on : 3000");
+});
